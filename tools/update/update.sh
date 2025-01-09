fc7f4bbc9172f676679eb50847e2fa1e1921dbdc
sunaoka <sunaoka@pocari.org>
2025-01-09 17:46:33 +0900

    Fixed src path

--- tools/update/update.sh
+++ tools/update/update.sh
@@ -2,6 +2,6 @@
 
 readonly ARCHIVE='https://github.com/sunaoka/aws-sdk-php-structures-generator/archive/refs/heads/main.tar.gz'
 
-rm -rf ../src/*
+rm -rf ../../src/*
 
-curl -sL -o - "${ARCHIVE}" | tar zxf - --strip=1 -C ../src
+curl -sL -o - "${ARCHIVE}" | tar zxf - --strip=1 -C ../../src
