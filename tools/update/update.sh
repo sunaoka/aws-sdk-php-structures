77539bddbb9d56278c24a7d0dbf397935d269472
sunaoka <sunaoka@pocari.org>
2025-01-09 17:53:49 +0900

    Update update.sh

--- tools/update/update.sh
+++ tools/update/update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -ue
+
 readonly ARCHIVE='https://github.com/sunaoka/aws-sdk-php-structures-generator/archive/refs/heads/main.tar.gz'
 
-rm -rf ../../src/*
+script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+base_dir="$(dirname "$(dirname "${script_dir}")")"
 
-curl -sL -o - "${ARCHIVE}" | tar zxf - --strip=1 -C ../../src
+rm -rf "${base_dir}"/src/*
+
+curl -sL -o - "${ARCHIVE}" | tar zxf - --strip=1 -C "${base_dir}"/src
